10291196d75b831a13c299488f82199ba8210af8
nwil850 <nwil850@aucklanduni.ac.nz>
2024-06-20 23:30:02 +1200

    Graph updated

--- src/Runner.py
+++ src/Runner.py
@@ -2,19 +2,15 @@ import matplotlib.pyplot as plt
 from Main import main
 import numpy as np
 
-sims = 1000
-
+sims = 10000
 result = []
 
 for i in range(0, sims):
     result.append(main())
 
+fig, axs = plt.subplots(2)
+fig.suptitle('Non-Logged vs Logged distribution')
+axs[0].hist(result, bins=50)
+axs[1].hist(np.log(result), bins=50)
 
-# non logged data
-plt.hist(result, bins=20)
-plt.show()
-
-# logged data
-resultln = np.log(result)
-plt.hist(resultln, bins=20)
 plt.show()
